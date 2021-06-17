0994dcd8d82160c1677532f713b8b3f522a5e3eb
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-06-17 20:31:36 +0300

    HW-5  Deploy-on-gh-pages

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {NavLink} from 'react-router-dom'
+import s from './Header.module.css'
+import { PATH } from './Routes'
 
 function Header() {
+    const [toggle, setToggle] = useState(true)
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+                <NavLink to={PATH.PRE_JUNIOR} className={s.link} activeClassName={s.activeLink}>pre-junior</NavLink>
+                <NavLink to={PATH.JUNIOR} className={s.link} activeClassName={s.activeLink}>junior</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={s.link} activeClassName={s.activeLink}>junior-plus</NavLink>
+                <span className={s.toggleBlock} onClick={ () => setToggle(true)}/>
+            {toggle && <span className={s.toggleBlock} onClick={() => setToggle(false)}/>}
         </div>
     )
 }
