503f65d5de190b2a2c466d8b7029c3c19b43c261
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-10-25 18:16:28 +0300

    HW-9  Clock and date

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,35 +1,43 @@
 import React, {useState} from 'react'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
+import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0)
-    const [date, setDate] = useState<Date>()
+    const [date, setDate] = useState<Date>(new Date)
     const [show, setShow] = useState<boolean>(false)
 
     const stop = () => {
-        // stop
+        clearInterval(timerId);
     }
     const start = () => {
         stop()
         const id: number = window.setInterval(() => {
-            // setDate
+            setDate(new Date)
         }, 1000)
         setTimerId(id)
     }
 
     const onMouseEnter = () => {
-        // show
+        setShow(true)
     }
     const onMouseLeave = () => {
-        // close
+        setShow(false)
     }
 
-    const stringTime = 'Time' // fix with date
-    const stringDate = 'Date' // fix with date
+
+    const stringTime = `${date.getHours() < 10 ? `0${date.getHours()}` : date.getHours()} :
+        ${date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()} :
+        ${date.getSeconds() < 10 ? `0${date.getSeconds()}` : date.getSeconds()}`
+
+    const stringDate = `${date.getFullYear()}.`+ `${date.getMonth() + 1 < 10 ? `0${date.getMonth() + 1}` : date.getMonth() + 1}.`+
+        `${date.getDate()< 10 ? `0${date.getDate()}` : date.getDate()}`
 
     return (
-        <div>
+        <div className={s.clockWrap}>
+            <div className={s.dateClock}>
             <div
+                className={s.clock}
                 onMouseEnter={onMouseEnter}
                 onMouseLeave={onMouseLeave}
             >
@@ -41,9 +49,10 @@ function Clock() {
                     {stringDate}
                 </div>
             )}
+            </div>
 
-            <SuperButton onClick={start}>start</SuperButton>
-            <SuperButton onClick={stop}>stop</SuperButton>
+            <SuperButton  onClick={start}>start</SuperButton>
+            <SuperButton  onClick={stop}>stop</SuperButton>
 
         </div>
     )
