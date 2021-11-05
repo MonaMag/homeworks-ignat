b4038082721678f389c1723eb947fafc8451b66d
Mona Mag <64916820+MonaMag@users.noreply.github.com>
2021-11-05 12:01:34 +0300

    HW-12.2 new style for menu

--- src/p2-homeworks/h5/Header.tsx
+++ src/p2-homeworks/h5/Header.tsx
@@ -1,18 +1,17 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {NavLink} from 'react-router-dom'
 import s from './Header.module.css'
 import {PATH} from './Routes'
 
 function Header() {
     return (
-        <div className={s.header}>
-                <nav className={s.menu}>
-                    <NavLink to={PATH.PRE_JUNIOR}  activeClassName={s.menuActive}>PreJunior</NavLink>
-                    <NavLink to={PATH.JUNIOR}  activeClassName={s.menuActive}>Junior</NavLink>
-                    <NavLink to={PATH.JUNIOR_PLUS} activeClassName={s.menuActive}>JuniorPlus</NavLink>
-                    <span className={s.toggleBlock}/>
-                </nav>
-
+        <div>
+            <nav className={s.menu}>
+                <NavLink to={PATH.PRE_JUNIOR} className={s.link} activeClassName={s.activeLink}>PRE_JUNIOR</NavLink>
+                <NavLink to={PATH.JUNIOR} className={s.link} activeClassName={s.activeLink}>JUNIOR</NavLink>
+                <NavLink to={PATH.JUNIOR_PLUS} className={s.link} activeClassName={s.activeLink}>JUNIOR_PLUS</NavLink>
+                <div className={s.toggle}/>
+            </nav>
         </div>
     )
 }
